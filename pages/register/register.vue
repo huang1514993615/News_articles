02099e0ca15e39e26e63a5f02244ba006ac64185
2804027886 <2804027886@qq.com>
2020-04-29 22:47:38 +0800

    我的，登录，注册页面第一次上传

---
+++ pages/register/register.vue
@@ -0,0 +1,31 @@
+<template>
+	<view>
+		<form @submit="" @reset="">
+			<text class="user">用户名</text>
+			<input type="text" value="" />
+			<text class="phone">手机号</text>
+			<input type="text" value="" />
+			<text class="password">密 码</text>
+			<input type="text" password="true" />
+			<button form-type="submit">注册</button>
+			<button form-type="reset">重置</button>
+		</form>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		methods: {
+			
+		}
+	}
+</script>
+
+<style>
+
+</style>
